e6b6ff284e0c48826e2f02d1110a424f03a956f2
Silver.Lee <68718384+godtechwak@users.noreply.github.com>
2022-07-12 09:45:19 +0900

    Update DB_Connection.py

--- script/DBConnection/DB_Connection.py
+++ script/DBConnection/DB_Connection.py
@@ -1,1 +1,27 @@
+import mysql.connector
+import sys
+import os
+import warnings
 
+sys.path.append('D:\Python\py_220712\config')
+import config
+
+os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
+
+warnings.filterwarnings('ignore')
+
+try:
+    conn =  mysql.connector.connect(
+        host=config.DB_CONNECTION_INFO['ENDPOINT'], 
+        user=config.DB_CONNECTION_INFO['USER'], 
+        passwd=config.DB_CONNECTION_INFO['USER'], 
+        port=config.DB_CONNECTION_INFO['PORT'], 
+        database=config.DB_CONNECTION_INFO['DBNAME']
+    )
+    
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE test (Seq int, Name VARCHAR(100), PRIMARY KEY(Seq));")
+    conn.commit()
+    conn.close()
+except Exception as e:
+    print("Database connection failed due to {}".format(e))
