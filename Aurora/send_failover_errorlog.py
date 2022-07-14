c66ab3a5e387c9c0571d4c1a259618f830238486
Silver.Lee <68718384+godtechwak@users.noreply.github.com>
2022-07-14 12:32:57 +0900

    Update send_failover_errorlog.py

--- Aurora/send_failover_errorlog.py
+++ Aurora/send_failover_errorlog.py
@@ -1,9 +1,10 @@
 import boto3
 import math
 import sys
-sys.path.append('D:\Python\Toy_Project\Aurora_Failover_Error_Message\config')
+sys.path.append('/home/ec2-user/config')
 import config
 from datetime import datetime, timedelta
+import time
 
 client_rds=boto3.client('rds', config.CON_INFO['region'])
 client_log=boto3.client('logs', config.CON_INFO['region'])
@@ -16,14 +17,14 @@ def get_failover_date():
 
         # It contains the time before and after 2 minutes of the failover action.
         date_time=[(categories['Date']+timedelta(minutes=-2), categories['Date']+timedelta(minutes=2)) for categories in events['Events'] for backup in categories['EventCategories'] if backup=='failover']
-        
+
         return date_time
 
-def get_db_instance_n_log_event(*befter_datetime):
+def get_db_instance_n_log_event(befter_datetime):
         global db_instance_writer_id
 
-        b_datetime=befter_datetime[0]
-        a_datetime=befter_datetime[1]
+        a_datetime=befter_datetime[0][0]
+        b_datetime=befter_datetime[1][1]
 
         db_cluster=client_rds.describe_db_clusters()
 
@@ -32,19 +33,23 @@ def get_db_instance_n_log_event(*befter_datetime):
 
         response=client_log.get_log_events(
             logGroupName=config.CON_INFO['loggroup_name'],
-            logStreamName=db_instance_writer_id,
-            startTime=math.trunc(a_datetime.timestamp()),
-            endTime=math.trunc(b_datetime.timestamp())
+            logStreamName=db_instance_writer_id[0],
+            startTime=(math.trunc(time.mktime(a_datetime.timetuple()))*1000),
+            endTime=(math.trunc(time.mktime(b_datetime.timetuple()))*1000)
         )
-        
+
         #[print(events['message']) for events in response['events']]
         with open("failover_{}.txt".format(datetime.strftime(datetime.now(),'%Y-%m-%d-%H-%M-%S')), "w") as f:
                 [f.write(events['message']) for events in response['events']]
-                        
+
 
 if __name__ == "__main__":
         date_time=datetime.now()
         db_instance_writer_id=[]
 
         get_failover_date()
-        get_db_instance_n_log_event(date_time)
+
+        if date_time==[]:
+            exit()
+        else:
+            get_db_instance_n_log_event(date_time)
